052ee6765fa06e21eddd77c76ef3fa05333ec056
Wahyu Wiguna <dirayuda5@gmail.com>
2026-06-30 10:24:36 +0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,33 @@
+
+const display =
+document.getElementById(
+"display"
+);
+
+function append(value){
+display.value += value;
+}
+
+function clearDisplay(){
+display.value = "";
+}
+
+function calculate(){
+
+try{
+
+display.value =
+eval(
+display.value
+);
+
+}
+
+catch{
+
+display.value =
+"Error";
+
+}
+
+}
